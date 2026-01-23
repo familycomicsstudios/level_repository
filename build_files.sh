80a4c1de0dc0b2afad140e40dd61279f4d5c8615
familycomicsstudios <themadpunter@proton.me>
2026-01-22 23:47:46 -0600

    ab

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 # Build script for Vercel deployment
 echo "Installing dependencies..."
-pip install -r requirements.txt
+pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
