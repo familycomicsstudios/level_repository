92d10ff1e14ebc0bd97470309ba3074e54751bf5
familycomicsstudios <themadpunter@proton.me>
2026-04-27 16:40:20 -0500

    Preview update

--- level_repository/settings.py
+++ level_repository/settings.py
@@ -218,3 +218,11 @@ CORS_ALLOWED_ORIGINS = [
     'https://turbowarp.org',
     'https://workshop.appelgame.net',
 ]
+
+# Allow common localhost development origins (include ports explicitly)
+CORS_ALLOWED_ORIGINS += [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+]
